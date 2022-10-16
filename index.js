5c9af574d6dbd9d0c6b090a89e013a67476e018c
Juan David <juandhernandez1215@gmail.com>
2022-10-16 18:14:12 -0500

    segundo

---
+++ index.js
@@ -0,0 +1,25 @@
+
+import express from 'express';
+import routes from './src/routes/Routes.js';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
+const __dirname = path.resolve();
+var app = express();
+const PORT=4000;
+var dbURL= 'mongodb+srv://Admin:SYU9PsDhAxK4GZo9@example.aswx27u.mongodb.net/?retryWrites=true&w=majority'
+
+//mongoose.Promise= global.Promise;
+mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true})
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+routes(app);
+app.get('/',(req,res)=>{
+  //console.log(__dirname)
+  //res.sendfile('app.html');}
+  res.send('I´m an API')}
+)
+
+app.listen(PORT, ()=> {
+  console.log(`i'm in port ${PORT}`);
+})
