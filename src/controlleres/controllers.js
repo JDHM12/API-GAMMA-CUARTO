89fbb543c29f0e4a57acd6300015018cb909edab
Juan David Hernández M <100490749+JDHM12@users.noreply.github.com>
2022-10-24 08:20:00 -0500

    Actualización Modelo

--- src/controlleres/controllers.js
+++ src/controlleres/controllers.js
@@ -3,9 +3,13 @@ import { RolloSchema } from '../models/models.js';
 
 const Rollo = mongoose.model('Rollo',RolloSchema);
 
+/*el valor de fecha de salida se calcula tomando la fecha de ingreso y sumandole las horas arrojadas por el modelo, la funcion parser convierte los valores de la suma 
+en milisegundo a formato fecha, de esta manera se obtiene este dato*/
+
 export const addRollo = (req,res)=>{
     let newRollo = new Rollo(req.body)
-      newRollo['fechaSalida']=Date.parse(new Date(Date.now()+(19.83*newRollo["durezaSalida"]-19.53*newRollo["durezaEntrada"]+1.15*newRollo["elevado"]+0.2*newRollo["familiaRef"]+0.5*newRollo["zona"]+3.97*newRollo["hueco"])*3.6e+6))
+      newRollo['fechaSalida']=Date.parse(new Date(Date.now()+(19.83*newRollo["durezaSalida"]-19.53*newRollo["durezaEntrada"]+1.15*newRollo["elevado"]
+                                                              +0.2*newRollo["familiaRef"]+0.5*newRollo["zona"]+3.97*newRollo["hueco"])*3.6e+6))
       newRollo.save((err,rollo)=>{
           if(err){
            res.send(err);
